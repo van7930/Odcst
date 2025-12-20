6df574d6ae03ff87cbb90f60fbca64c952ece849
van7930 <van79303@gmail.com>
2025-12-20 21:08:43 +0700

    update ui

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,7 @@ function App(){
     return(
     <div class="layout">
         <div class="title"> 
-            <h1>A System for Managing Enterprise-Student Collaborations at <span class="highlight">UTH</span> on Real-World Projects</h1>
+            <h1>Managing Enterprise-Student Collaborations at <span class="highlight">UTH</span> on Real-World Projects</h1>
         </div>
         <Login/>
     </div>
