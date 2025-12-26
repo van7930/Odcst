2ae53962bd1b1fe2458ce56070cd61d7b4da3abe
van7930 <van79303@gmail.com>
2025-12-26 16:35:46 +0700

    update code

--- src/login.jsx
+++ src/login.jsx
@@ -3,17 +3,11 @@ import logoUTH from "./assets/logoUTH.png";
 function Login({setUser}){
     let email="";
     let password="";
+    let showPasswor=false;
+    let remember=false;
     function handleLogin(event){
-        event.preventDefault();
-        if(!email||!password){
-            console.log("please enter full information");
-        }
-        if(email==="odcgroup@gmail.com" && password==="12345678"){
-            console.log("login successful");
-        } else {
-            console.log("invalid email or password");
-        }
-    }
+        console.log("login attemp:", {email,password,remember});
+        };
     return(     
         <div class="lg">
             <img src={logoUTH} class="logoUTH"/>
